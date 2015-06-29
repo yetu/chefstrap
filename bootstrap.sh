faa58a8a3f18e0a90235d97e486f56a4eae117a2
Blake Irvin <bixu@users.noreply.github.com>
2015-06-29 20:10:20 +0200

    more accurate OS check

--- bootstrap.sh
+++ bootstrap.sh
@@ -7,13 +7,13 @@ do
   esac
 done
 
-if [ $(grep -c "Ubuntu" /etc/product) -ge 1 ]; then
+if [ $(grep -c Ubuntu /etc/product) -ge 1 ]; then
   apt-get update
   apt-get install -y wget ca-certificates
   curl -s -L https://chef.io/chef/install.sh | bash -s -- -v $version
 fi
 
-if [ $(grep -c "Joyent Instance" /etc/product) -ge 1 ]; then
+if [ $(grep -c SmartOS /etc/release) -ge 1 ]; then
   chef_package=$(pkgin search chef | grep "chef-[0-9]" | head -n1 | cut -d" " -f1)
   pkgin -y install $chef_package build-essential
   gem install chef --version "$version" --no-ri --no-rdoc
