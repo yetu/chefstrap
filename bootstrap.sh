725f1b55dd62b45047e97f5c55b0b4128b0e764f
Blake Irvin <bixu@users.noreply.github.com>
2015-06-29 16:32:41 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ if [ $(grep -c Ubuntu /etc/product) -ge 1 ]; then
   curl -s -L https://chef.io/chef/install.sh | bash -s -- -v $VERSION
 fi
 
-if [ $(grep smartos /etc/product) -ge 1 ]; then
+if [ $(grep -c smartos /etc/product) -ge 1 ]; then
   pkgin -y install ruby215-chef build-essential
   gem install chef --version "$VERSION" --no-ri --no-rdoc
 fi
